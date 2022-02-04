9fe7a7700ca8d3515c7cd71f71b6753c6a0acdb7
Bryan Martins <bryanmaraujo544@gmail.com>
2022-02-04 08:17:36 -0300

    adding procfile

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,6 @@ app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(5000, () => console.log('Server is running on port 5000'));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server is running on port 5000'));
